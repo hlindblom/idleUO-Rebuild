659539e1832eec4f052baf5854b3e3db0bbecb10
Holden Lindblom <holden.lindblom@gmail.com>
2023-08-22 22:27:46 -0400

    automatic notifications with messages

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`server running: ${port}`);
+});
---
+++ public/alerts.js
@@ -0,0 +1,269 @@
+window.alerts = [
+  {
+    type: "News",
+    message:
+      "Mage tower in Britain struck by mysterious lightning! Magic users baffled.",
+  },
+  {
+    type: "Alert",
+    message: "Gold thefts on the rise! Beware of shadowy figures near banks.",
+  },
+  {
+    type: "Markets",
+    message:
+      "Price of leather sees unexpected surge due to high demand for armor.",
+  },
+  {
+    type: "News",
+    message:
+      "Serpent's Hold reports increase in sea serpent sightings. Travelers be wary!",
+  },
+  {
+    type: "Alert",
+    message: "Rare blood moon event tonight. Expect increased undead activity!",
+  },
+  {
+    type: "Markets",
+    message:
+      "Sudden flood in Minoc mines disrupts silver extraction. Prices skyrocket!",
+  },
+  {
+    type: "News",
+    message: "Skara Brae faces food shortage after mysterious crop blight.",
+  },
+  {
+    type: "Alert",
+    message:
+      "Rogue bard causing chaos! Citizens advised to avoid hypnotic tunes.",
+  },
+  {
+    type: "Markets",
+    message:
+      "The introduction of new mounts causes a sharp rise in feed prices.",
+  },
+  {
+    type: "News",
+    message: "Guild wars declared! High tensions reported between top guilds.",
+  },
+  {
+    type: "Alert",
+    message:
+      "Ancient wyrm sighted near Cove. Citizens advised to stay indoors!",
+  },
+  {
+    type: "Markets",
+    message:
+      "Spike in demand for enchanted weapons causes shortage of magical essences.",
+  },
+  {
+    type: "News",
+    message: "Orc raids intensify around the Compassion desert.",
+  },
+  {
+    type: "Alert",
+    message: "Portal anomalies detected! Mages warn of unstable teleportation.",
+  },
+  {
+    type: "Markets",
+    message: "New jewelry designs lead to increased demand for pearls.",
+  },
+  {
+    type: "News",
+    message: "Gargoyles and humans broker peace treaty in Ter Mur.",
+  },
+  {
+    type: "Alert",
+    message: "Beware! Juka warriors spotted in Ilshenar.",
+  },
+  {
+    type: "Markets",
+    message: "New tailoring techniques push up the price of silk.",
+  },
+  {
+    type: "News",
+    message:
+      "Shipwrecks discovered off the coast of Nujel'm. Treasure hunters rejoice!",
+  },
+  {
+    type: "Alert",
+    message: "Merchants report a dire rat infestation in their storerooms!",
+  },
+  {
+    type: "Markets",
+    message:
+      "Fine wines in high demand for upcoming festivals. Vineyards can't keep up!",
+  },
+  {
+    type: "News",
+    message:
+      "Mysterious monolith appears in Felucca. Adventurers flock to decipher.",
+  },
+  {
+    type: "Alert",
+    message:
+      "Mage duels disrupt daily life in Moonglow. Authorities intervene!",
+  },
+  {
+    type: "Markets",
+    message:
+      "Discovery of new herb drives potion innovation, reagent market affected.",
+  },
+  {
+    type: "News",
+    message:
+      "Famous tamer goes missing after seeking rare beast in Tokuno Islands.",
+  },
+  {
+    type: "Alert",
+    message:
+      "Necromancer cult suspected to be behind recent graveyard disturbances.",
+  },
+  {
+    type: "Markets",
+    message: "Introduction of clockwork gadgets boosts copper sales.",
+  },
+  {
+    type: "News",
+    message:
+      "The town of Paws faces water contamination. Citizens urged to boil water.",
+  },
+  {
+    type: "Alert",
+    message: "New dungeon discovered! But beware of its deadly traps.",
+  },
+  {
+    type: "Markets",
+    message: "Fur prices drop as warmer seasons approach.",
+  },
+  {
+    type: "News",
+    message: "Elf-Human music festival to be held in Heartwood!",
+  },
+  {
+    type: "Alert",
+    message: "Massive spider infestation reported in the forests of Yew.",
+  },
+  {
+    type: "Markets",
+    message: "Rise in shipbuilding causes spike in canvas and rope prices.",
+  },
+  {
+    type: "News",
+    message:
+      "Dragon turtle migration observed. Fishermen advised to avoid certain sea routes.",
+  },
+  {
+    type: "Alert",
+    message: "Mysterious fog envelopes Magincia. Locals report eerie voices.",
+  },
+  {
+    type: "Markets",
+    message: "Obsidian prices surge following new mage staff trend.",
+  },
+  {
+    type: "News",
+    message: "Unicorn sightings bring joy and hope to the citizens of Trammel.",
+  },
+  {
+    type: "Alert",
+    message: "Increase in werewolf sightings during the full moon! Stay safe.",
+  },
+  {
+    type: "Markets",
+    message: "Beekeeping boom causes surplus in honey, prices plummet.",
+  },
+  {
+    type: "News",
+    message: "Rumors of a hidden city beneath the sands of the Lost Lands.",
+  },
+  {
+    type: "Alert",
+    message: "Reports of haunted armor attacking adventurers in Doom.",
+  },
+  {
+    type: "Markets",
+    message: "Alchemy breakthrough causes gold prices to soar.",
+  },
+  {
+    type: "News",
+    message: "The Meer announce a grand library project in their homeland.",
+  },
+  {
+    type: "Alert",
+    message:
+      "Strange lights spotted over Shame dungeon. Extraterrestrial activity suspected.",
+  },
+  {
+    type: "Markets",
+    message: "Volcano eruption in Fire Island leads to obsidian rush.",
+  },
+  {
+    type: "News",
+    message: "Annual pet competition to be held in Tamer's Delight next week.",
+  },
+  {
+    type: "Alert",
+    message:
+      "Mysterious disappearances in dungeons. Adventurers, be on your guard!",
+  },
+  {
+    type: "Markets",
+    message: "Price of glass surges due to stained glass window trend.",
+  },
+  {
+    type: "News",
+    message: "The Council of Mages announces a month-long arcane symposium.",
+  },
+  {
+    type: "Alert",
+    message: "Lizardman uprising threatens the peace of the Lost Lands.",
+  },
+  {
+    type: "News",
+    message: "Mysterious disappearance of town criers; Moonglow hardest hit!",
+  },
+  {
+    type: "Alert",
+    message:
+      "Rumors of a dragon nesting near Trinsic causing panic among citizens!",
+  },
+  {
+    type: "Markets",
+    message:
+      "Unprecedented demand for reagents leads to a 10% spike in Mandrake Root prices!",
+  },
+  {
+    type: "News",
+    message: "Droughts in Yew lead to shortages of vital herbs and potions.",
+  },
+  {
+    type: "Alert",
+    message:
+      "Major serverwide event announced! The Ghosts of the Lost King set to haunt dungeons next week!",
+  },
+  {
+    type: "Markets",
+    message:
+      "Lumber prices soar as rumors circulate of a treant uprising in the Spiritwood Forest.",
+  },
+  {
+    type: "News",
+    message:
+      "Vesper blacksmiths strike over the price of ingots; Crafting halts!",
+  },
+  {
+    type: "Alert",
+    message:
+      "Famous pirate Captain Goldbeard reportedly hiding a treasure near Buccaneer's Den!",
+  },
+  {
+    type: "Markets",
+    message:
+      "Surge in magical artifacts pushes gemstone prices to an all-time high!",
+  },
+  {
+    type: "News",
+    message:
+      "Council warns of increased lich activity in the ancient ruins; advises players to prepare for combat!",
+  },
+];
